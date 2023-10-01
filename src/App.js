5b5e0eb78d57151b21e5d796d66cfe68b402904f
jcpiont <jcpiont@gmail.com>
2023-10-01 12:04:51 -0400

    Initial game creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import "./App.css";
 
-function App() {
+const numRows = 20;
+const cellSize = 20;
+
+const App = () => {
+  const [snake, setSnake] = useState([{ x: 5, y: 5 }]);
+  const [food, setFood] = useState({ x: 10, y: 10 });
+  const [dir, setDir] = useState({ x: 0, y: 0 });
+
+  const moveSnake = useCallback(() => {
+    let newSnake = snake.map(pos => ({ ...pos }));
+    let head = Object.assign({}, newSnake[0]);
+
+    head.x += dir.x;
+    head.y += dir.y;
+
+    if (head.x === food.x && head.y === food.y) {
+      setFood({
+        x: Math.floor(Math.random() * numRows),
+        y: Math.floor(Math.random() * numRows)
+      });
+      newSnake = [head, ...newSnake];
+    } else {
+      newSnake.pop();
+      newSnake = [head, ...newSnake];
+    }
+
+    setSnake(newSnake);
+  }, [dir, food, snake]);
+
+  useEffect(() => {
+    const handleKeydown = e => {
+      switch (e.key) {
+        case "ArrowUp":
+          setDir({ x: 0, y: -1 });
+          break;
+        case "ArrowDown":
+          setDir({ x: 0, y: 1 });
+          break;
+        case "ArrowLeft":
+          setDir({ x: -1, y: 0 });
+          break;
+        case "ArrowRight":
+          setDir({ x: 1, y: 0 });
+          break;
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeydown);
+    return () => document.removeEventListener("keydown", handleKeydown);
+  }, []);
+
+  useEffect(() => {
+    const gameInterval = setInterval(moveSnake, 150);
+    return () => clearInterval(gameInterval);
+  }, [moveSnake]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="GameBoard">
+        {Array.from({ length: numRows }).map((_, y) =>
+          Array.from({ length: numRows }).map((_, x) => (
+            <div
+              key={`cell-${x}-${y}`}
+              className={`cell ${snake.some(pos => pos.x === x && pos.y === y) ? "snake" : ""} ${food.x === x && food.y === y ? "food" : ""}`}
+              style={{
+                width: `${cellSize}px`,
+                height: `${cellSize}px`
+              }}
+            ></div>
+          ))
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
